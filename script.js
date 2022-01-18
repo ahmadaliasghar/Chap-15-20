b751337d2726e823ed571748d30b48ec2b782e59
ahmadaliasghar <88629200+ahmadaliasghar@users.noreply.github.com>
2022-01-18 19:59:23 +0500

    Part 2

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("statement").innerHTML = "<p>Hello</p>"
+
+document.getElementById("ifElseIf").onclick = function() {
+
+    document.getElementById("statement").innerHTML
+}
